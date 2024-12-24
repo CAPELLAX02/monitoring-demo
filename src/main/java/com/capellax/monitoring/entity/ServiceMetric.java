7b7198231c4d658f00105f78ad00b13e26a02c83
CAPELLAX02 <ahmtatar@gmail.com>
2024-12-24 19:38:08 +0300

    Add Lombok dependency & Create entites

---
+++ src/main/java/com/capellax/monitoring/entity/ServiceMetric.java
@@ -0,0 +1,31 @@
+package com.capellax.monitoring.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceMetric {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private MonitoredService monitoredService;
+
+    private String metricName;
+
+    private Double value;
+
+    private LocalDateTime timestamp;
+
+}
